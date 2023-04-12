25fb2a49c0f1fd932688dba37652dae4c8a21fa9
atharvapatil123 <atharva7275@gmail.com>
2023-04-12 20:12:11 +0530

    Added algos endpoints in fastapi

--- preprocessing/app.py
+++ preprocessing/app.py
@@ -11,9 +11,14 @@ from matplotlib import pyplot as plt
 from neo4j import GraphDatabase
 import networkx as nx
 
-uri = "bolt://localhost:7687"
-user = "neo4j"
-password = "Pablo123"
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env.
+
+uri = os.environ.get("NEO4J_URI")
+user = os.environ.get("NEO4J_USERNAME")
+password = os.environ.get("NEO4J_PASSWORD")
+
+
 driver = GraphDatabase.driver(uri, auth=(user, password))
 
 
@@ -232,6 +237,172 @@ async def preprocess_csv_files(files: list[UploadFile], bankNames: list[str], ac
     # do some additional preprocessing on the final dataframe here
     result_csv = result_df.to_csv(index=False)
     return {"result_csv": result_csv}
+
+
+@app.get("/centralities")
+async def cycles():
+    print("Centralities")
+    # Define Cypher query
+    cypher_query = """
+    MATCH (sender:Bank)-[t:TRANSACTION]->(receiver:Bank)
+    RETURN sender.id AS SenderBankID, receiver.id AS ReceiverBankID, 
+        t.txnDate AS TxnDate, t.valueDate AS ValueDate, 
+        t.description AS Description, t.refNo AS RefNoChequeNo, 
+        t.debit AS Debit, t.credit AS Credit, t.balance AS Balance
+    """
+
+    # Create empty graph
+    G = nx.DiGraph()
+
+    # Open Neo4j session
+    with driver.session() as session:
+        # Run query and iterate over result
+        result = session.run(cypher_query)
+        for record in result:
+            # Extract data from record
+            sender_bank_id = record["SenderBankID"]
+            receiver_bank_id = record["ReceiverBankID"]
+            txn_date = record["TxnDate"]
+            value_date = record["ValueDate"]
+            description = record["Description"]
+            ref_no_cheque_no = record["RefNoChequeNo"]
+            debit = record["Debit"]
+            credit = record["Credit"]
+            balance = record["Balance"]
+
+            # Add sender and receiver nodes to graph
+            G.add_node(sender_bank_id)
+            G.add_node(receiver_bank_id)
+
+            # Add transaction edge to graph
+            G.add_edge(sender_bank_id, receiver_bank_id, 
+                    txn_date=txn_date, value_date=value_date, 
+                    description=description, ref_no_cheque_no=ref_no_cheque_no, 
+                    debit=debit, credit=credit, balance=balance)
+            
+    print("==========Centrality Measures==========")
+    # degree centrality
+    dc = []
+    deg_cen = nx.degree_centrality(G)
+    print("Degree centrality:", deg_cen)
+    dc.append(deg_cen)
+
+    # closeness centrality
+    cc = []
+    clo_cen = nx.closeness_centrality(G)
+    print("Closeness centrality:", clo_cen)
+    cc.append(clo_cen)
+
+    # betweenness centrality
+    b_c=[]
+    bet_cen = nx.betweenness_centrality(G)
+    print("Betweenness centrality:", bet_cen)
+    b_c.append(bet_cen)
+
+    #eigenvector centrality
+    ec = []
+    # eig_cen = nx.eigenvector_centrality(G)
+    # print("Eigenvector centrality:", eig_cen)
+    # ec.append(eig_cen)
+
+    return {"deg_cen": dc, "clo_cen": cc, "bet_cen": b_c, "eig_cen": ec}
+
+
+@app.get("/hits")
+async def hits():
+    print("Hits")
+    # Define Cypher query
+    cypher_query = """
+    MATCH (sender:Bank)-[t:TRANSACTION]->(receiver:Bank)
+    RETURN sender.id AS SenderBankID, receiver.id AS ReceiverBankID, 
+        t.txnDate AS TxnDate, t.valueDate AS ValueDate, 
+        t.description AS Description, t.refNo AS RefNoChequeNo, 
+        t.debit AS Debit, t.credit AS Credit, t.balance AS Balance
+    """
+
+    # Create empty graph
+    G = nx.DiGraph()
+
+    # Open Neo4j session
+    with driver.session() as session:
+        # Run query and iterate over result
+        result = session.run(cypher_query)
+        for record in result:
+            # Extract data from record
+            sender_bank_id = record["SenderBankID"]
+            receiver_bank_id = record["ReceiverBankID"]
+            txn_date = record["TxnDate"]
+            value_date = record["ValueDate"]
+            description = record["Description"]
+            ref_no_cheque_no = record["RefNoChequeNo"]
+            debit = record["Debit"]
+            credit = record["Credit"]
+            balance = record["Balance"]
+
+            # Add sender and receiver nodes to graph
+            G.add_node(sender_bank_id)
+            G.add_node(receiver_bank_id)
+
+            # Add transaction edge to graph
+            G.add_edge(sender_bank_id, receiver_bank_id, 
+                    txn_date=txn_date, value_date=value_date, 
+                    description=description, ref_no_cheque_no=ref_no_cheque_no, 
+                    debit=debit, credit=credit, balance=balance)
+            
+    print("=============HITS==============")
+    h, a = nx.hits(G)
+
+    # print authorities and hubs
+    print("Authorities: ", a)
+    print("Hubs: ", h)
+
+    return {"Hubs": h, "Authorities": a}
+
+@app.get("/cycles")
+async def cycles():
+    print("Cycles")
+    # Define Cypher query
+    cypher_query = """
+    MATCH (sender:Bank)-[t:TRANSACTION]->(receiver:Bank)
+    RETURN sender.id AS SenderBankID, receiver.id AS ReceiverBankID, 
+        t.txnDate AS TxnDate, t.valueDate AS ValueDate, 
+        t.description AS Description, t.refNo AS RefNoChequeNo, 
+        t.debit AS Debit, t.credit AS Credit, t.balance AS Balance
+    """
+
+    # Create empty graph
+    G = nx.DiGraph()
+
+    # Open Neo4j session
+    with driver.session() as session:
+        # Run query and iterate over result
+        result = session.run(cypher_query)
+        for record in result:
+            # Extract data from record
+            sender_bank_id = record["SenderBankID"]
+            receiver_bank_id = record["ReceiverBankID"]
+            txn_date = record["TxnDate"]
+            value_date = record["ValueDate"]
+            description = record["Description"]
+            ref_no_cheque_no = record["RefNoChequeNo"]
+            debit = record["Debit"]
+            credit = record["Credit"]
+            balance = record["Balance"]
+
+            # Add sender and receiver nodes to graph
+            G.add_node(sender_bank_id)
+            G.add_node(receiver_bank_id)
+
+            # Add transaction edge to graph
+            G.add_edge(sender_bank_id, receiver_bank_id, 
+                    txn_date=txn_date, value_date=value_date, 
+                    description=description, ref_no_cheque_no=ref_no_cheque_no, 
+                    debit=debit, credit=credit, balance=balance)
+            
+    print("========CYCLES=======")
+    accounts = list(nx.simple_cycles(G))
+
+    return {"accounts": accounts}
 
 @app.get("/pageRank")
 async def pageRank():
